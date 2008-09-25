ba09a96243d6414b972a103f26a2511438cf9843
Giuseppe "denever" Martino <martinogiuseppe@gmail.com>
2008-09-25 08:34:28 +0200

    Used threading.Event Removed debug printing Added stop() method Added get_result method

--- pgns2tracelib/tracemanager.py
+++ pgns2tracelib/tracemanager.py
@@ -25,7 +25,7 @@
 ###########################################################################
 
 import Queue
-from threading import Thread
+from threading import Thread, Event
 from pgns2tracelib.tracesql import TraceSql
 from pgns2tracelib.tracesql import create_db_from_trace
 
@@ -37,95 +37,117 @@ class TraceManager(Thread):
         self.running = True
         self.operations = Queue.Queue()
         self.results = Queue.Queue()
+        self.evnt_new_item = Event()
+        self.evnt_new_db = Event()
         
     def open_db(self, db_name):
         self.db_name = db_name
+        self.evnt_new_db.set()
 
     def query_nodes(self):
         if self.tracesql:
             self.operations.put( (self.tracesql.get_nodes, None) )
+            self.evnt_new_item.set()
         
     def query_flows(self):
         if self.tracesql:
             self.operations.put( (self.tracesql.get_flows, None) )
-
+            self.evnt_new_item.set()
+            
     def query_flow_types(self):
         if self.tracesql:
             self.operations.put( (self.tracesql.get_flow_types, None) )
-        
+            self.evnt_new_item.set()
+            
     def query_pkt_iptypes(self):
         if self.tracesql:
             self.operations.put( (self.tracesql.get_pkt_iptypes, None) )
-        
+            self.evnt_new_item.set()
+            
     def query_src_dst_per_flow(self):        
         if self.tracesql:
             self.operations.put( (self.tracesql.get_src_dst_per_flow, None) )
-
+            self.evnt_new_item.set()
+            
     def query_flow_types(self):
         if self.tracesql:
             self.operations.put( (self.tracesql.get_flow_types, None) )
-
+            self.evnt_new_item.set()
+            
     def query_trace_of(self, unique_id):
         if self.tracesql:
             self.operations.put( (self.tracesql.get_trace_of, unique_id) )
-
+            self.evnt_new_item.set()
+            
     def query_recv_pkts_num_at_node(self, node_id):
         if self.tracesql:
             self.operations.put( (self.tracesql.count_recv_pkt_at_node, node_id) )
-
+            self.evnt_new_item.set()
+            
     def query_pkts_at_macdst(self, mac_dest):
         if self.tracesql:
             self.operations.put( (self.tracesql.get_pkts_at_macdst, mac_dest) )
-
+            self.evnt_new_item.set()
+            
     def query_pkts_at_lvl(self, lvl):
         if self.tracesql:
             self.operations.put( (self.tracesql.get_pkts_at_lvl, lvl) )                               
-
+            self.evnt_new_item.set()
+            
     def query_pkts_flowid(self, flowid, lvl):
         if self.tracesql:
             self.operations.put( (self.tracesql.get_pkts_flowid, flowid, lvl) )
-
+            self.evnt_new_item.set()
+            
     def query_sent_pkts_times_at(self, node_id, flow_id, lvl):
         if self.tracesql:
             self.operations.put( (self.tracesql.get_sent_pkts_times_at, node_id, flow_id, lvl) )
-
+            self.evnt_new_item.set()
+            
     def query_recv_pkts_times_at(self, node_id, flow_id, lvl):
         if self.tracesql:
             self.operations.put( (self.tracesql.get_recv_pkts_times_at, node_id, flow_id, lvl) )
-
+            self.evnt_new_item.set()
+            
     def query_recv_flow_total_size_at(self, node_id, flow_id, hdr_size, lvl):
         if self.tracesql:
             self.operations.put( (self.tracesql.get_recv_flow_total_size_at, node_id, flow_id, hdr_size, lvl) )
-
+            self.evnt_new_item.set()
+            
     def query_sent_bursts_per_flow(self, lvl):
         if self.tracesql:
             self.operations.put( (self.tracesql.get_sent_bursts_per_flow, lvl) )
+            self.evnt_new_item.set()
+            
+    def get_result(self):
+        return self.results.get()
 
-    def get_result():
-        return results.get()
-
-    def stop():
+    def stop(self):
         self.running = False
+        self.evnt_new_item.set()
 
     def run(self):
         while self.running:
-            if not self.tracesql and self.db_name:
+            if not self.tracesql:
+                self.evnt_new_db.wait()
                 self.tracesql = TraceSql(self.db_name)
-                print 'opened db', self.db_name
-                # emit opened db
+
             if self.tracesql:
-                try:
-                    operation = self.operations.get()
-                except:
-                    continue
+                if self.operations.empty():
+                    self.evnt_new_item.wait()
+                    self.evnt_new_item.clear()
+                    
+                    if self.running == False:
+                        break
+
+                operation = self.operations.get()
 
                 if operation[1] != None:
-                    print operation[1:]
                     result = operation[0]([arg for arg in operation[1:] ])
-                    results.put(result)
+                    self.results.put(result)
                 else:
                     result = operation[0]()
-                    results.put(result)
+                    self.results.put(result)
 
                 print result
 
